7b66950d64f30ea9e1d98fb61eeac56d6adf497c
Michel Jacobs <micheljacobs@hotmail.nl>
2018-09-10 15:09:17 +0200

    added more assingment 2b stuff

---
+++ Assets/Scripts/DiceUvMapper.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class DiceUvMapper : MonoBehaviour
+{
+    [SerializeField]
+    private Vector2[] uvMapp;
+
+    private Mesh mesh;
+
+    private Vector2[] diceMap;
+
+	void Start ()
+	{
+	    mesh = GetComponent<MeshFilter>().mesh;
+
+	    uvMapp = mesh.uv;
+
+	    var third = 1f / 3f;
+	    var twoThird = 2f / 3f;
+
+	    List<Vector2> face = new List<Vector2>
+        {
+            new Vector2(0 , 0),
+            new Vector2(0 , third),
+            new Vector2(third , 0),
+
+            new Vector2(third , 0),
+            new Vector2(third , third),
+            new Vector2(0 , third),
+        };
+
+        face.ForEach(x => Debug.Log(x));
+
+	    Debug.Log("asdasd");
+
+        face.Skip(1).Concat(face.Take(1)).ToList().ForEach(x => Debug.Log(x));
+
+	    List<Vector2> list = new List<Vector2>();
+        //1
+        list.AddRange(face.Select(f => {
+            f.x += twoThird;
+            f.y += third;
+            return f;
+        }).ToList());
+
+        //2
+	    list.AddRange(face.Select(f => {
+	        f.x += 0;
+	        f.y += 0;
+	        return f;
+	    }).ToList());
+
+        //3
+	    list.AddRange(face.Select(f => {
+	        f.x += third;
+	        f.y += 0;
+	        return f;
+	    }).ToList());
+
+        //4
+	    list.AddRange(face.Select(f => {
+	        f.x += 0;
+	        f.y += third;
+	        return f;
+	    }).ToList());
+
+        //5
+	    list.AddRange(face.Select(f => {
+	        f.x += twoThird;
+	        f.y += 0;
+	        return f;
+	    }).ToList());
+
+        //6
+	    list.AddRange(face.Select(f => {
+	        f.x += third;
+	        f.y += third;
+	        return f;
+	    }).ToList());
+
+	    mesh.uv = list.ToArray();
+	}
+}
--- Assets/Scripts/ScrollTexture.cs
+++ Assets/Scripts/ScrollTexture.cs
@@ -9,46 +9,73 @@ public class ScrollTexture : MonoBehaviour
     private void Start()
     {
         material.mainTextureOffset = new Vector2();
+
+//        GetComponent<Renderer>().material.te
+
+        material.mainTexture.wrapMode = TextureWrapMode.Repeat;
+        material.mainTexture.filterMode = FilterMode.Bilinear;
+        material.mainTexture.anisoLevel = 0;
+
     }
 
-    void Update ()
-	{
-	    material.mainTextureOffset += new Vector2(0.1f * Time.deltaTime, 0);
+    void Update()
+    {
+        material.mainTextureOffset += new Vector2(0.1f * Time.deltaTime, 0);
+        material.mainTextureOffset = new Vector2(material.mainTextureOffset.x % 2, 0);
 
-	    if (Input.GetKeyUp(KeyCode.A))
-	    {
-	        switch (material.mainTexture.wrapMode)
-	        {
-	            case TextureWrapMode.Clamp:
-	                material.mainTexture.wrapMode = TextureWrapMode.Mirror;
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            switch (material.mainTexture.wrapMode)
+            {
+                case TextureWrapMode.Clamp:
+                    material.mainTexture.wrapMode = TextureWrapMode.Mirror;
                     Debug.Log(TextureWrapMode.Mirror);
-	                break;
-	            case TextureWrapMode.Mirror:
-	                material.mainTexture.wrapMode = TextureWrapMode.MirrorOnce;
-	                Debug.Log(TextureWrapMode.MirrorOnce);
                     break;
-	            case TextureWrapMode.MirrorOnce:
-	                material.mainTexture.wrapMode = TextureWrapMode.Repeat;
-	                Debug.Log(TextureWrapMode.Repeat);
+                case TextureWrapMode.Mirror:
+                    material.mainTexture.wrapMode = TextureWrapMode.MirrorOnce;
+                    Debug.Log(TextureWrapMode.MirrorOnce);
                     break;
-	            case TextureWrapMode.Repeat:
-	                material.mainTexture.wrapMode = TextureWrapMode.Clamp;
-	                Debug.Log(TextureWrapMode.Clamp);
+                case TextureWrapMode.MirrorOnce:
+                    material.mainTexture.wrapMode = TextureWrapMode.Repeat;
+                    Debug.Log(TextureWrapMode.Repeat);
                     break;
-	            default:
-	                material.mainTexture.wrapMode = TextureWrapMode.Mirror;
-	                break;
-	        }
-	    }
-
-	    if (Input.GetKeyUp(KeyCode.S))
-	    {
-
-	    }
-
-	    //        material.mainTexture.wrapMode = 
-            //        material.mainTexture.filterMode = 
-            //	    material.mainTexture.anisoLevel = 
-
+                case TextureWrapMode.Repeat:
+                    material.mainTexture.wrapMode = TextureWrapMode.Clamp;
+                    Debug.Log(TextureWrapMode.Clamp);
+                    break;
+                default:
+                    material.mainTexture.wrapMode = TextureWrapMode.Mirror;
+                    break;
+            }
         }
+
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            switch (material.mainTexture.filterMode)
+            {
+                case FilterMode.Point:
+                    material.mainTexture.filterMode = FilterMode.Bilinear;
+                    Debug.Log(FilterMode.Bilinear);
+                    break;
+                case FilterMode.Bilinear:
+                    material.mainTexture.filterMode = FilterMode.Trilinear;
+                    Debug.Log(FilterMode.Trilinear);
+                    break;
+                case FilterMode.Trilinear:
+                    material.mainTexture.filterMode = FilterMode.Point;
+                    Debug.Log(FilterMode.Point);
+                    break;
+                default:
+                    material.mainTexture.filterMode = FilterMode.Bilinear;
+                    break;
+            }
+        }
+
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            material.mainTexture.anisoLevel += 1;
+            material.mainTexture.anisoLevel = material.mainTexture.anisoLevel % 10;
+            Debug.Log(material.mainTexture.anisoLevel);
+        }
+    }
 }
