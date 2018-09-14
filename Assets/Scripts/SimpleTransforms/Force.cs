88c09f33ffbaad6bffdb07fba4597bcf20169f3d
Michel Jacobs <micheljacobs@hotmail.nl>
2018-09-14 09:47:17 +0200

    finnished 3b

---
+++ Assets/Scripts/SimpleTransforms/Force.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Force : MonoBehaviour
+{
+    public Vector3 ForceDirection = Vector3.up;
+
+    public float ForcePower = 100;
+
+    private float _localPower;
+    private Rigidbody _rigidbody;
+
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+
+        
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            _rigidbody.AddForce(ForceDirection.normalized * ForcePower);
+        }
+    }
+}
